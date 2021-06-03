bc455cea567421e38a86d5fa4769b0cf6ad2f139
Rafael Alves <rafael.rac.mg@gmail.com>
2021-06-03 08:57:35 -0300

    feat: elk basic configuration

--- pkg/infra/db.go
+++ pkg/infra/db.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 	"log"
 
-	_ "github.com/lib/pq"
+	"go.elastic.co/apm/module/apmsql"
+	_ "go.elastic.co/apm/module/apmsql/pq"
 )
 
 func GetConnection(host string, port int, user, password, dbName string) (*sql.DB, error) {
 	connectionString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbName)
-	db, err := sql.Open("postgres", connectionString)
+	db, err := apmsql.Open("postgres", connectionString)
 	if err != nil {
 		log.Printf("error while connect to database: %v", err)
 		return nil, err
